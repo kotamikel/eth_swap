4a0576c935379df8610de1c9b7cc20ba23abb4b5
Mikel Kota <mikelkota@gmail.com>
2021-10-27 22:00:00 -0400

    CREATE FIRST SMART CONTRACT. CREATE MIGRATION TO DEPLOY SMART CONTRACT.

---
+++ src/contracts/EthSwamp.sol
@@ -0,0 +1,8 @@
+pragmaa solidity ^0.5.0;
+
+contract EthSwap {
+	// Statically typed language - string variable
+	// Public - a function on the smart contract called name which will return that value. 
+	string public name = "EthSwap Instant Exchange";
+
+}
