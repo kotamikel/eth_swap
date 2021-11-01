ff931504808aa9d6bac07ea7d62838c912c0d626
Mikel Kota <mikelkota@gmail.com>
2021-11-01 17:34:25 -0400

    SC1 EthSwap - Add sellTokens function + tests. Modified the configuration to allow code to compile.

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -11,7 +11,14 @@ contract EthSwap {
 	uint public rate = 100;
 
 	// Event definition
-	event TokenPurchased(
+	event TokensPurchased(
+		address account,
+		address token,
+		uint amount,
+		uint rate
+	);
+
+	event TokensSold(
 		address account,
 		address token,
 		uint amount,
@@ -24,11 +31,14 @@ contract EthSwap {
 		token = _token;   // This will store it to the STATE VARIABLE and not local variable (_token)
 	}
 
-	// Public - callable outside the SC
+	// Public - callable outside the  SC
 	// Payable - send ether whenever we call fn
 	function buyTokens() public payable {
 		// Calculate the number of tokens to buy
 		uint tokenAmount = msg.value * rate; // msg.value - global variable telling us how much ETHER was sent when the fn was called
+
+		// Require that EthSwap has enough tokens
+		require(token.balanceOf(address(this)) >= tokenAmount); 
 
 		// Transfer the token from EthSwap to whoever is buying it
 		// arg1 - msg is a global variable in solidity - sender is value of address calling this function
@@ -36,6 +46,25 @@ contract EthSwap {
 		token.transfer(msg.sender, tokenAmount);
 
 		// Emit an event. 
-		emit TokenPurchased(msg.sender, address(token), tokenAmount, rate);
+		emit TokensPurchased(msg.sender, address(token), tokenAmount, rate);
+	}
+
+	function sellTokens(uint _amount) public {
+		// User can't sell more tokens than they have
+		require(token.balanceOf(msg.sender) >= _amount);
+
+		// Calculate amount of Ether to redeem
+		uint etherAmount = _amount / rate; 
+		
+		// Require that EthSwap has enough Ether
+		require(address(this).balance >= etherAmount);
+
+		// THE SC CANNOT CALL THE TRANSFER FN ON ERC20 TOKEN ON BEHALF OF INVESTOR. (you can hide transfer fns)
+		// ERC20 HAS SPECIAL FN CALLED - TRANSFERFROM - This fn is used to have SC spend tokens for you. 
+		token.transferFrom(msg.sender, address(this), _amount);
+		msg.sender.transfer(etherAmount);
+
+		// Emit an event. 
+		emit TokensSold(msg.sender, address(token), _amount, rate);
 	}
 }
