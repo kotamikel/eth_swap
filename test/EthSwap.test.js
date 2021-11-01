ad180da1a15961a02ad9a9bd4401a5bae55761d5
Mikel Kota <mikelkota@gmail.com>
2021-11-01 12:03:50 -0400

    Added tests and SC 1 and 2. EthSwap & DApp token.

---
+++ test/EthSwap.test.js
@@ -0,0 +1,49 @@
+// Import SC
+const Token = artifacts.require("Token");
+const EthSwap = artifacts.require("EthSwap");
+
+//CHAI TEST FRAMEWORK - Configure assertions
+require('chai')
+	.use(require('chai-as-promised'))
+	.should()
+
+// Make the DApp Token Human readable
+function tokens(n) {
+	// Convert token to WEI
+	return web3.utils.toWei(n, "ether");
+}
+//Test
+contract('EthSwap', (accounts) => {
+	//make these variables available in the other fn
+	let token, ethSwap
+
+	// Create a BEFORE HOOK to avoid repeating code. 
+	before(async () => {
+		token = await Token.new()
+		ethSwap = await EthSwap.new()
+		await token.transfer(ethSwap.address, tokens('1000000'))
+	})
+
+	//Test Token was deployed
+	describe('Token deployment', async () => {
+		it('contract has a name', async () => {
+			const name = await token.name()
+			assert.equal(name, 'DApp Token')
+		})
+	})
+
+	//Test EthSwap was deployed
+	describe('EthSwap deployment', async () => {
+		it('contract has a name', async () => {
+			const name = await ethSwap.name()
+			assert.equal(name, 'EthSwap Instant Exchange')
+		})
+		it('contract has tokens', async () => {
+			let balance = await token.balanceOf(ethSwap.address)
+			assert.equal(balance.toString(), tokens('1000000'))
+
+		})
+	})
+
+	//Test to see if the contract HAS TOKENS
+})
